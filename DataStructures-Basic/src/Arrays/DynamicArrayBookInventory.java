ee6180676202c9c140f641227447debb251601b9
Shrinivas Ambiger <86651980+osemdeveloper@users.noreply.github.com>
2023-06-28 17:15:43 +0530

    Dynamic-Array

--- DataStructures-Basic/src/Arrays/DynamicArrayBookInventory.java
+++ DataStructures-Basic/src/Arrays/DynamicArrayBookInventory.java
@@ -2,10 +2,10 @@ package Arrays;
 
 import java.util.Arrays;
 
-class Book{
+class Book {
 	private String title;
 	private String author;
-	
+
 	public Book(String title, String author) {
 		super();
 		this.title = title;
@@ -19,40 +19,53 @@ class Book{
 	public String getAuthor() {
 		return author;
 	}
-	
-	
-	
+
 }
 
 public class DynamicArrayBookInventory {
-	
+
 	private Book[] books;
 	private int size;
 	private int capacity;
-	
+
 	public DynamicArrayBookInventory() {
-		capacity= 10;
+		capacity = 10;
 		books = new Book[capacity];
-		size=0;
+		size = 0;
 	}
-	
+
 	public void addBook(Book book) {
-		if(size == capacity) {
-			
+		if (size == capacity) {
+			increaseCapacity();
 		}
+		books[size] = book;
+		size++;
 	}
-	
+
 	private void increaseCapacity() {
 		capacity *= 2;
 		books = Arrays.copyOf(books, capacity);
 	}
-	
+
 	public void printInventory() {
 		System.out.println("Bookstore Inventory: ");
-		for(int i=0; i< size; i++) {
+		for (int i = 0; i < size; i++) {
 			Book book = books[i];
 			System.out.println("Title: " + book.getTitle() + "Author: " + book.getAuthor());
 		}
+	}
+
+	public static void main(String[] args) {
+		DynamicArrayBookInventory inventory = new DynamicArrayBookInventory();
+
+		// Adding books to the inventory
+
+		inventory.addBook(new Book("Java Programming", "Simon Robert"));
+		inventory.addBook(new Book("Data Structures and Algorithms", "Alice Johnson"));
+		inventory.addBook(new Book("Introduction to Machine Learning", "David Brown"));
+
+		// printing the inventory
+		inventory.printInventory();
 	}
 
 }
