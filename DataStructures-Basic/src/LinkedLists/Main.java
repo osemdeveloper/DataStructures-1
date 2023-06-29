952a3126da83a7fd75ec280dff9f0aeca377ba99
Shrinivas Ambiger <86651980+osemdeveloper@users.noreply.github.com>
2023-06-29 20:06:58 +0530

    format

--- DataStructures-Basic/src/LinkedLists/Main.java
+++ DataStructures-Basic/src/LinkedLists/Main.java
@@ -3,84 +3,83 @@ package LinkedLists;
 import java.util.Stack;
 
 class ExpressionNode {
-    String value;
-    ExpressionNode next;
+	String value;
+	ExpressionNode next;
 
-    public ExpressionNode(String value) {
-        this.value = value;
-        this.next = null;
-    }
+	public ExpressionNode(String value) {
+		this.value = value;
+		this.next = null;
+	}
 }
 
 class ExpressionList {
-    ExpressionNode head;
+	ExpressionNode head;
 
-    public void insert(String value) {
-        ExpressionNode newNode = new ExpressionNode(value);
+	public void insert(String value) {
+		ExpressionNode newNode = new ExpressionNode(value);
 
-        if (head == null) {
-            head = newNode;
-        } else {
-            newNode.next = head;
-            head = newNode;
-        }
-    }
+		if (head == null) {
+			head = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
 
-    public int evaluatePostfix() {
-        Stack<Integer> stack = new Stack<>();
+	public int evaluatePostfix() {
+		Stack<Integer> stack = new Stack<>();
 
-        ExpressionNode current = head;
-        while (current != null) {
-            if (isNumeric(current.value)) {
-                stack.push(Integer.parseInt(current.value));
-            } else {
-                int operand2 = stack.pop();
-                int operand1 = stack.pop();
+		ExpressionNode current = head;
+		while (current != null) {
+			if (isNumeric(current.value)) {
+				stack.push(Integer.parseInt(current.value));
+			} else {
+				int operand2 = stack.pop();
+				int operand1 = stack.pop();
 
-                int result;
-                switch (current.value) {
-                    case "+":
-                        result = operand1 + operand2;
-                        break;
-                    case "-":
-                        result = operand1 - operand2;
-                        break;
-                    case "*":
-                        result = operand1 * operand2;
-                        break;
-                    case "/":
-                        result = operand1 / operand2;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Invalid expression");
-                }
-                stack.push(result);
-            }
-            current = current.next;
-        }
+				int result;
+				switch (current.value) {
+				case "+":
+					result = operand1 + operand2;
+					break;
+				case "-":
+					result = operand1 - operand2;
+					break;
+				case "*":
+					result = operand1 * operand2;
+					break;
+				case "/":
+					result = operand1 / operand2;
+					break;
+				default:
+					throw new IllegalArgumentException("Invalid expression");
+				}
+				stack.push(result);
+			}
+			current = current.next;
+		}
 
-        return stack.pop();
-    }
+		return stack.pop();
+	}
 
-    private boolean isNumeric(String str) {
-        return str.matches("-?\\d+");
-    }
+	private boolean isNumeric(String str) {
+		return str.matches("-?\\d+");
+	}
 }
 
 public class Main {
-    public static void main(String[] args) {
-        ExpressionList expressionList = new ExpressionList();
-        
-        // Inserting expressions in postfix notation
-        expressionList.insert("5");
-        expressionList.insert("7");
-        expressionList.insert("2");
-        expressionList.insert("*");
-        expressionList.insert("+");
-        
-        // Evaluating and printing the result
-        int result = expressionList.evaluatePostfix();
-        System.out.println("Result: " + result); // Output: Result: 19
-    }
-}
+	public static void main(String[] args) {
+		ExpressionList expressionList = new ExpressionList();
 
+		// Inserting expressions in postfix notation
+		expressionList.insert("5");
+		expressionList.insert("7");
+		expressionList.insert("2");
+		expressionList.insert("*");
+		expressionList.insert("+");
+
+		// Evaluating and printing the result
+		int result = expressionList.evaluatePostfix();
+		System.out.println("Result: " + result); // Output: Result: 19
+	}
+}
